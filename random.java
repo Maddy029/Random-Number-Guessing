4238e1110744decba5fdce45ef76178019a7df80
Maddy029 <118113208+Maddy029@users.noreply.github.com>
2025-08-07 14:29:37 +0530

    Update random.java

--- random.java
+++ random.java
@@ -29,17 +29,3 @@ public class Main {
       scanner.close();
 }
 }
-/*fizz buzz
-import java.util.*;
-public class Main {
-    public static void main (String args[]){
-        for (int i=0;i<=100;i++){
-            if(i%3==0){
-                System.out.println("FIZZ");
-            }
-            else if(i%==)
-            
-        }
-
- }
-}*/
